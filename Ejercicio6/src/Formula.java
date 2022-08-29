ea82f9fb24a6b9f89085d12f90d25e09c0363ede
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-08-29 08:55:25 -0700

    Ejercicio 6 empezado

---
+++ Ejercicio6/src/Formula.java
@@ -0,0 +1,11 @@
+public class Formula {
+    private double x, y, z, w, f;
+
+    public Formula(double x, double y, double z, double w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+    
+}
